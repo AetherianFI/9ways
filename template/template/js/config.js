5693428cafed1bfd225d8c6bf93560c67dba506b
AetherianFI <saarinenotso@gmail.com>
2024-02-06 13:07:18 +0200

    Added basic website layout

---
+++ template/template/js/config.js
@@ -0,0 +1,13 @@
+const center = {
+   lat: 9.07,
+   lng: 7.466,
+   text: 'Abuja, Nigeria'
+}
+
+const hereCredentials = {
+   id: 'YOUR-HERE-ID',
+   code: 'YOUR-HERE-CODE',
+   apikey: 'YOUR-API-KEY'
+}
+
+export { center, hereCredentials };
