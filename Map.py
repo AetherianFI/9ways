c0766cab8e9894fcc1b2ced388a0187cb3b5096a
AetherianFI <saarinenotso@gmail.com>
2024-02-12 12:05:11 +0200

    Added busstops.json

--- Map.py
+++ Map.py
@@ -2,11 +2,23 @@ import folium
 from folium.plugins import LocateControl
 import branca
 
-min_lon, max_lon = 
-min_lat, max_lat = , 13.514
+min_lon, max_lon = 6.611773, 7.914522
+min_lat, max_lat = 8.306199, 9.527200
 
 # Base map, location of the mid-point & zoom
-map = folium.Map(location= [9.072264, 7.491302], zoom_start=12)
+map = folium.Map(
+    location=[9.072264, 7.491302],
+    zoom_start=12,
+    min_lat=min_lat,
+    max_lat=max_lat,
+    min_lon=min_lon,
+    max_lon=max_lon,
+)
+
+folium.CircleMarker([max_lat, min_lon], tooltip="Upper Left Corner").add_to(map)
+folium.CircleMarker([min_lat, min_lon], tooltip="Lower Left Corner").add_to(map)
+folium.CircleMarker([min_lat, max_lon], tooltip="Lower Right Corner").add_to(map)
+folium.CircleMarker([max_lat, max_lon], tooltip="Upper Right Corner").add_to(map)
 
 # Alternative tile theme for folium.Map
 # tiles="Cartodb Positron"
@@ -24,11 +36,15 @@ html = """
 
 # Popup
 iframe = branca.element.IFrame(html=html, width=400, height=200)
-popup=folium.Popup(iframe, max_width=500)
+popup = folium.Popup(iframe, max_width=400)
 
 # feature group & adding the popup text to marker
-fg = folium.FeatureGroup(name= "My Map")
-fg.add_child(folium.Marker(location=[9.072264, 7.491302], popup=popup, icon= folium.Icon(color= 'red')))
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(
+    folium.Marker(
+        location=[9.072264, 7.491302], popup=popup, icon=folium.Icon(color="red")
+    )
+)
 map.add_child(fg)
 
 # Gps tracking request through browser
