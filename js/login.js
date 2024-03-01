62c78f2bd0b770ac167f7716295f3d6911e130aa
AetherianFI <saarinenotso@gmail.com>
2024-03-02 00:19:11 +0200

    added js file for checking login form

--- js/login.js
+++ js/login.js
@@ -1,15 +1,26 @@
-function checkForm() {
-    var username = document.getElementById("username").value;
+function checkLogin() {
+    event.preventDefault()
+
+    var username = document.getElementById("username").value
     var password = document.getElementById("password").value
 
-    if (username == "" || password == "") {
-        alert("You have to fill both username and password!")
-        console.log("Form did not submit succesfully")
-
-        return false;
-    } else {
-        console.log("Form submitted succesfully")
-        return true;
-    }
-
+    fetch("../accounts.json")
+        .then(response => response.json())
+        .then(data => {
+            if (data.hasOwnProperty(username)) {
+                if (data[username] === password) {
+                    window.location.href = "../html/map.html"
+                } else {
+                    alert("Wrong password")
+                }
+            } else {
+                alert("Username not found")
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching JSON:", error)
+        })
 }
+
+// Add event listener to the form
+document.getElementById("loginForm").addEventListener("submit", checkLogin);
