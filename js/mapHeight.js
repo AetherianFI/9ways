046de2973af37749137eb589999d228ae9d7c81e
AetherianFI <saarinenotso@gmail.com>
2024-03-03 03:10:46 +0200

    Update mapHeight.js

--- js/mapHeight.js
+++ js/mapHeight.js
@@ -1,3 +1,4 @@
+// Checks if the user is logged in right now and updated the map height so that page doesn't become scrollable
 function checkHeight() {
     if (localStorage.getItem("loggedInUser")) {
         document.getElementById("map").style.height = "89.9%";
