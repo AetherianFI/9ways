abb9bdab8ab5dfda3caae0f948345f84a79d613d
aetherian0 <saarinenotso@gmail.com>
2024-03-08 12:57:00 +0200

    Update register.js

--- js/register.js
+++ js/register.js
@@ -4,12 +4,7 @@ registrationForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const formData = new FormData(registrationForm);
-    const data = {};
-
-    // Iterate over each key-value pair in formData and store them in the data object
-    formData.forEach((value, key) => {
-        data[key] = value;
-    });
+    const data = Object.fromEntries(formData);
 
     fetch("http://localhost:3000/saveFormData", {
         method: "POST",
@@ -18,7 +13,7 @@ registrationForm.addEventListener("submit", (event) => {
         },
         body: JSON.stringify(data)
     })
-        .then(res => res.json())
-        .then(data => console.log(data))
+        .then(res => res.text()) // Parse response as text
+        .then(data => console.log(data)) // Log the text response
         .catch(error => console.log(error))
 });
