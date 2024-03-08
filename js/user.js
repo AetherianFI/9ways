373745d460a50c288c9b7ab528ac5b33ccd1ca08
aetherian0 <saarinenotso@gmail.com>
2024-03-08 16:04:59 +0200

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,6 +1,7 @@
 var username = localStorage.getItem("loggedInUser");
 var currentUser = document.getElementById("currentUser");
 
+// shows not logged in text if user isnt logged in and shows the username and settings when user is logged in
 if (!username || username === "null") {
     currentUser.textContent = "Not logged in";
 } else {
@@ -9,6 +10,7 @@ if (!username || username === "null") {
 
     currentUser.textContent = "Currently logged in as: " + username;
 
+    // Adds event listeneder to logout button that clears localstorage and redirects to login page when clicked
     var logoutButton = document.querySelector(".logout");
     logoutButton.addEventListener("click", () => {
         localStorage.clear();
