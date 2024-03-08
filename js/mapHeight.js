61f026829e0f61e0d8647cdbcffc2c2070e609ac
aetherian0 <saarinenotso@gmail.com>
2024-03-08 15:31:38 +0200

    Delete mapHeight.js

--- js/mapHeight.js
+++
@@ -1,8 +0,0 @@
-// Checks if the user is logged in right now and updated the map height so that page doesn't become scrollable
-function checkHeight() {
-    if (localStorage.getItem("loggedInUser")) {
-        document.getElementById("map").style.height = "91.7%";
-    }
-}
-
-checkHeight()
