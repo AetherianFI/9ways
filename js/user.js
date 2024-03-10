790053a1c865d9d8e056273baf0e1c64e98d21bd
aetherian0 <saarinenotso@gmail.com>
2024-03-10 23:52:04 +0200

    updated js and css for userpage

--- js/user.js
+++ js/user.js
@@ -2,11 +2,13 @@ var username = localStorage.getItem("loggedInUser");
 var currentUser = document.getElementById("loggedInUser");
 var notLoggedIn = document.querySelector(".notLoggedIn");
 var loggedIn = document.querySelector(".loggedIn");
+var accountCreated = document.getElementById("accountCreated");
 
 // shows not logged in text if user isnt logged in and shows the username and settings when user is logged in
 if (username) {
     notLoggedIn.style.display = "none";
     currentUser.textContent = "Currently logged in as: " + username;
+    accountCreated.textContent = "hei";
 
     // Adds event listeneder to logout button that clears localstorage and redirects to login page when clicked
     var logoutButton = document.getElementById("logoutButton");
