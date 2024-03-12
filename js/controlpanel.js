1bfb5e0581f67b2356f6ad09668e3ce62f4b80f5
aetherian0 <saarinenotso@gmail.com>
2024-03-12 16:05:39 +0200

    Update controlpanel.js

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -1,5 +1,16 @@
 // Goes through the POI.json and adds all busstops from there into the dropdown menu
 var drop_down_menu = document.getElementById("bus_stop_list");
-var elementti = document.createElement("option");
+
+fetch("../databases/POI.json")
+    .then((response) => response.json())
+    .then((data) => {
+        data.bus_stops.forEach((busStop) => {
+            var bus_stop_element = document.createElement("option");
+            bus_stop_element.textContent = busStop.name;
+            drop_down_menu.add(bus_stop_element);
+        });
+    });
+
+/*var elementti = document.createElement("option");
 elementti.textContent = "testi123";
-drop_down_menu.add(elementti);
+drop_down_menu.add(elementti);*/
