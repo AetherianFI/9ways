7203a80eceb9688e513c132e25b2b4d22d1a3418
aetherian0 <saarinenotso@gmail.com>
2024-03-12 21:50:10 +0200

    Update controlpanel.js

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -1,16 +1,14 @@
 // Goes through the POI.json and adds all busstops from there into the dropdown menu
 var drop_down_menu = document.getElementById("bus_stop_list");
 var timetables = document.getElementById("timetables");
-var index = 1;
 
 fetch("../databases/POI.json")
     .then((response) => response.json())
     .then((data) => {
         data.bus_stops.forEach((busStop) => {
             var bus_stop_element = document.createElement("option");
-            bus_stop_element.textContent = `${index}. ${busStop.name}`;
+            bus_stop_element.textContent = `${busStop.id}. ${busStop.name}`;
             drop_down_menu.add(bus_stop_element);
-            index = index + 1;
         });
 
         // Everytime new option is selected on the dropdown list it updated the selectedIndex variable and changed the timetable into correct one
