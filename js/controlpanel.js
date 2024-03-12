85b3924b78cdda9a69f537846539cc13845786a5
aetherian0 <saarinenotso@gmail.com>
2024-03-12 15:52:57 +0200

    Update controlpanel.js

---
+++ js/controlpanel.js
@@ -0,0 +1,5 @@
+// Goes through the POI.json and adds all busstops from there into the dropdown menu
+var drop_down_menu = document.getElementById("bus_stop_list");
+var elementti = document.createElement("option");
+elementti.textContent = "testi123";
+drop_down_menu.add(elementti);
