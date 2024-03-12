fac7e18812053feacf93a6c2d9ab47c98b1c8dbc
aetherian0 <saarinenotso@gmail.com>
2024-03-12 22:50:26 +0200

    Update controlpanel.js

--- js/controlpanel.js
+++ js/controlpanel.js
@@ -1,5 +1,6 @@
 var drop_down_menu = document.getElementById("bus_stop_list");
 var timetable = document.getElementById("timetable");
+var time_list = document.getElementById("time_selection");
 
 // Goes through the POI.json and adds all busstops from there into the dropdown menu
 fetch("../databases/POI.json")
@@ -17,6 +18,16 @@ fetch("../databases/POI.json")
             var busstop_timetable = data.bus_stops[selectedIndex].timetable;
             // Adds timetable to the element with "timetables" id and separates the values with space
             timetable.textContent = busstop_timetable.join(" ");
+
+            // Clears the previous times that have been added to the selection element
+            time_list.textContent = "";
+
+            // Goes through all the busstops from the selectedIndex busstop and adds them to selection element
+            data.bus_stops[selectedIndex].timetable.forEach((timetable) => {
+                var single_time = document.createElement("option");
+                single_time.textContent = `${timetable}`;
+                time_list.add(single_time);
+            });
         });
     });
 
