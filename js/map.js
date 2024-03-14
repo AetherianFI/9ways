d5a9ff959de9cef4b3d089da4ea188fe20fcd504
aetherian0 <saarinenotso@gmail.com>
2024-03-15 01:49:12 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,54 +2,54 @@
 var map = L.map("map", {
     center: [9.05, 7.491302],
     crs: L.CRS.EPSG3857,
-    maxBounds: [[8.8, 7.15], [9.2, 7.8]],
+    maxBounds: [
+        [8.8, 7.15],
+        [9.2, 7.8],
+    ],
     zoom: 14,
     zoomControl: true,
     preferCanvas: false,
-}
-);
+});
 
 // Choose tiles that the map uses and set min and max zoom
-var tileLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var tileLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     minZoom: 12,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-
 // Create layer for bus stops that Noa found
-var random_bus_stops = L.layerGroup().addTo(map)
-
+var random_bus_stops = L.layerGroup().addTo(map);
 
 // Create layer for bus stops from airport to city
-var airport_to_city = L.layerGroup().addTo(map)
-
+var airport_to_city = L.layerGroup().addTo(map);
 
 // Create busIcon
 var busIconRoute1 = L.icon({
-    iconUrl: '../img/icon4.png',
+    iconUrl: "../img/icon4.png",
     iconSize: [30, 30], // Set the size of the icon
     iconAnchor: [15, 30], // Set the anchor point of the icon
-    popupAnchor: [0, -30] // Set the popup anchor point
+    popupAnchor: [0, -30], // Set the popup anchor point
 });
 
 var busIconRoute2 = L.icon({
-    iconUrl: '../img/icon6.png',
+    iconUrl: "../img/icon6.png",
     iconSize: [30, 30], // Set the size of the icon
     iconAnchor: [15, 30], // Set the anchor point of the icon
-    popupAnchor: [0, -30] // Set the popup anchor point
+    popupAnchor: [0, -30], // Set the popup anchor point
 });
 
 var busIconRoute3 = L.icon({
-    iconUrl: '../img/icon8.png',
+    iconUrl: "../img/icon8.png",
     iconSize: [30, 30], // Set the size of the icon
     iconAnchor: [15, 30], // Set the anchor point of the icon
-    popupAnchor: [0, -30] // Set the popup anchor point
+    popupAnchor: [0, -30], // Set the popup anchor point
 });
 
 fetch("../databases/POI.json")
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
         // Process the JSON data and create markers
         // For example:
         data.bus_stops.forEach((busStop) => {
@@ -70,7 +70,7 @@ fetch("../databases/POI.json")
             var bus_marker = L.marker([busStop.latitude, busStop.longitude], {
                 icon: busIcon,
             });
-            
+
             if (busStop.route === 1) {
                 bus_marker.addTo(airport_to_city);
             } else if (busStop.route === 2) {
@@ -82,34 +82,30 @@ fetch("../databases/POI.json")
             bus_marker.bindPopup(popup);
         });
     })
-    .catch(error => console.error("Error loading JSON file:", error));
-
+    .catch((error) => console.error("Error loading JSON file:", error));
 
 // Add length ruler to bottom left corner
-var control_scale = L.control.scale().addTo(map)
-
+var control_scale = L.control.scale().addTo(map);
 
 // Create baselayer for bus stops
 var baseLayers = {
     "Airport to City": airport_to_city,
-    "Random Bus Stops": random_bus_stops
+    "Random Bus Stops": random_bus_stops,
 };
-
 
 // Add layer control so that markers can be toggled on & off
 var layer_control = L.control.layers(null, baseLayers).addTo(map);
 
-
 // Add geocoder control (Search bar)
 var geocoder = L.Control.geocoder({
-    position: 'topright',
+    position: "topright",
     expand: "hover",
 }).addTo(map);
 
-
 // Add current mouse coordinates to the map
-var mousePosition = L.control.mousePosition({ position: "bottomright" }).addTo(map);
-
+var mousePosition = L.control
+    .mousePosition({ position: "bottomright" })
+    .addTo(map);
 
 // Handle form submission
 /* var form = document.getElementById('routeForm');
@@ -143,30 +139,27 @@ form.addEventListener('submit', function (event) {
 
 // For button creation
 function createButton(label, container) {
-    var btn = L.DomUtil.create('button', '', container);
-    btn.setAttribute('type', 'button');
+    var btn = L.DomUtil.create("button", "", container);
+    btn.setAttribute("type", "button");
     btn.innerHTML = label;
     return btn;
 }
 
 // On click events
-map.on('click', function(e) {
-    var container = L.DomUtil.create('div'),
-        startBtn = createButton('Start from this location', container),
-        destBtn = createButton('Go to this location', container);
+map.on("click", function (e) {
+    var container = L.DomUtil.create("div"),
+        startBtn = createButton("Start from this location", container),
+        destBtn = createButton("Go to this location", container);
 
-    L.popup()
-        .setContent(container)
-        .setLatLng(e.latlng)
-        .openOn(map);
-    
-    L.DomEvent.on(startBtn, 'click', function() {
+    L.popup().setContent(container).setLatLng(e.latlng).openOn(map);
+
+    L.DomEvent.on(startBtn, "click", function () {
         routing.spliceWaypoints(0, 1, e.latlng);
         map.closePopup();
-        });
+    });
 
-    L.DomEvent.on(destBtn, 'click', function() {
+    L.DomEvent.on(destBtn, "click", function () {
         routing.spliceWaypoints(routing.getWaypoints().length - 1, 1, e.latlng);
         map.closePopup();
-        });
+    });
 });
