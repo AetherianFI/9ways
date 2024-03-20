002ba9db6d6e5233d7459baf6788b1e1842e66a9
MiskaTup <miskatuppurainen@gmail.com>
2024-03-20 15:24:18 +0200

    Update ticket.js

--- public/js/ticket.js
+++ public/js/ticket.js
@@ -3,22 +3,29 @@ function increment(index) {
   var totalElement = document.getElementById('total');
   var ticketTypeElement = document.getElementById('ticket-type');
 
+
+  if (ticketTypeElement.textContent !== '') {
+    return;
+  }
+
   var num = parseInt(numElement.textContent);
-  num++;
-  numElement.textContent = num;
+  if (num < 1) {
+    num++;
+    numElement.textContent = num;
 
-  var total = parseInt(totalElement.textContent);
-  total += index === 0 ? 200 : (index === 1 ? 200 : (index === 2 ? 15000 : 200));
-  totalElement.textContent = total;
+    var total = parseInt(totalElement.textContent);
+    total += index === 0 ? 200 : (index === 1 ? 200 : (index === 2 ? 15000 : 200));
+    totalElement.textContent = total;
 
-  if ((index === 0 || index === 1 || index === 2) && total > 0) {
+    if ((index === 0 || index === 1 || index === 2) && total > 0) {
       if (index === 0) {
-          ticketTypeElement.textContent = 'Day ticket';
+        ticketTypeElement.textContent = 'Day ticket';
       } else if (index === 1) {
-          ticketTypeElement.textContent = 'Single ticket';
+        ticketTypeElement.textContent = 'Single ticket';
       } else if (index === 2) {
-          ticketTypeElement.textContent = 'Monthly pass';
+        ticketTypeElement.textContent = 'Monthly pass';
       }
+    }
   }
 }
 
@@ -28,16 +35,16 @@ function decrement(index) {
   var ticketTypeElement = document.getElementById('ticket-type');
 
   var num = parseInt(numElement.textContent);
-  if (num > 0) {
-      num--;
-      numElement.textContent = num;
+  if (num > 0 && num <= 1) {
+    num--;
+    numElement.textContent = num;
 
-      var total = parseInt(totalElement.textContent);
-      total -= index === 0 ? 200 : (index === 1 ? 200 : (index === 2 ? 15000 : 200));
-      totalElement.textContent = total;
+    var total = parseInt(totalElement.textContent);
+    total -= index === 0 ? 200 : (index === 1 ? 200 : (index === 2 ? 15000 : 200));
+    totalElement.textContent = total;
 
-      if ((index === 0 || index === 1 || index === 2) && total === 0) {
-          ticketTypeElement.textContent = '';
-      }
+    if (total === 0) {
+      ticketTypeElement.textContent = '';
+    }
   }
 }
